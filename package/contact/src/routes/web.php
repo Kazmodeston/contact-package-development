df5ff2ee0e1507bcbf7d91bc5400a188bb934593
Muhammad Kazim <kazmodeston1@gmail.com>
2022-01-15 21:57:15 +0100

    restructured route file to point to Controller

--- package/contact/src/routes/web.php
+++ package/contact/src/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Skillshare\Contact\Http\Controllers\ContactsController;
 
-Route::get('/contact', function(){
-    return view("contact::contact");
-});
+
+Route::get('/contact', [ContactsController::class, 'index'])->name('contact');
+Route::post('/contact', [ContactsController::class, 'sendMessage']);
+
+
