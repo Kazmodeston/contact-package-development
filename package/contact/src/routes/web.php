b54db24bf81e0f56a0b5c7baf02201af85c537e8
Muhammad Kazim <kazmodeston1@gmail.com>
2022-01-15 22:10:19 +0100

    added web middleware for the routes to inherite properties of the root route

--- package/contact/src/routes/web.php
+++ package/contact/src/routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Skillshare\Contact\Http\Controllers\ContactsController;
 
+Route::group(['middleware' => ['web']], function(){
 
-Route::get('/contact', [ContactsController::class, 'index'])->name('contact');
-Route::post('/contact', [ContactsController::class, 'sendMessage']);
-
+    Route::get('/contact', [ContactsController::class, 'index'])->name('contact');
+    Route::post('/contact', [ContactsController::class, 'sendMessage']);
+    
+});
 
