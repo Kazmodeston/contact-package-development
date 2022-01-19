e7a07324b11775fe8b4b76718755814ab4a290b8
Muhammad Kazim <kazmodeston1@gmail.com>
2022-01-19 11:37:38 +0100

    created function for Event and include Observer into boot method of the ContactEvent

---
+++ package/contact/src/EventContactServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Skillshare\Contact;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
+use Skillshare\Contact\Models\Contact;
+use Skillshare\Contact\Observers\ContactObserver;
+
+class EventContactServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array<class-string, array<int, class-string>>
+     */
+    protected $listen = [
+        Registered::class => [
+            SendEmailVerificationNotification::class,
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Contact::observe(ContactObserver::class);
+    }
+}
